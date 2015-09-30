b5f715b6d71fc3faece94b53ac8499581a00bdcc
Jelle Bloemsma <jelle.bloemsma@gmail.com>
2015-09-30 13:54:10 +0200

    new file:   BoardConfigVendor.mk 	new file:   bin/6620_launcher 	new file:   bin/6620_wmt_concurrency 	new file:   bin/6620_wmt_lpbk 	new file:   bin/MtkCodecService 	new file:   bin/aal 	new file:   bin/aee 	new file:   bin/akmd8963 	new file:   bin/akmd8975 	new file:   bin/ami304d 	new file:   bin/atci_service 	new file:   bin/atcid 	new file:   bin/audiocmdservice_atci 	new file:   bin/autokd 	new file:   bin/bmm050d 	new file:   bin/ccci_fsd 	new file:   bin/ccci_mdinit 	new file:   bin/dualmdlogger 	new file:   bin/em_svr 	new file:   bin/emdlogger1 	new file:   bin/geomagneticd 	new file:   bin/gsm0710muxd 	new file:   bin/gsm0710muxdmd2 	new file:   bin/guiext-server 	new file:   bin/immvibed 	new file:   bin/magd 	new file:   bin/matv

---
+++ zp999-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zopo/zp999/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zopo/zp999/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zopo/zp999/etc,system/etc)
+
+# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zopo/zp999/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zopo/zp999/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zopo/zp999/vendor,system/vendor)
